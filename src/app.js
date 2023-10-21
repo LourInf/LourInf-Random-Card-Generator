966e0a0561916832a9d52d7a3298ed4e2bb8b4a1
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-10-21 22:57:55 +0000

    wip

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardsArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+  //Selects random card from possibleCards array and sets HTML content of the element with class ".number" to the selected card
+  let randomNumber = cardsArray[Math.floor(Math.random() * cardsArray.length)];
+
+  document.querySelector(".number").innerHTML = randomNumber;
+
+  //select random suit for the suit on top
+  let suitsArray = ["♦", "♥", "♠", "♣"];
+  let randomSuit = suitsArray[Math.floor(Math.random() * suitsArray.length)];
+
+  let chosenSuit = (document.querySelector(".suit-top").innerHTML = randomSuit);
+
+  //bottom suit needs to match suit on top
+  let sameSuitBottom = (document.querySelector(
+    ".suit-bottom"
+  ).innerHTML = chosenSuit);
+
+  /*let colorArray = ["#FF0000", "#000000"];
+  let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+  document.querySelector(".suit-top").innerHTML = randomColor;*/
 };
