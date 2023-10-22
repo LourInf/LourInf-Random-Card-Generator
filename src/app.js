1b34ad80a0b0778d425b7acc2d6f0474127e01ea
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-10-22 20:43:42 +0000

    add buttom functionality

--- src/app.js
+++ src/app.js
@@ -4,26 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  let cardsArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
-  //Selects random card from possibleCards array and sets HTML content of the element with class ".number" to the selected card
-  let randomNumber = cardsArray[Math.floor(Math.random() * cardsArray.length)];
+  let showRandomCard = () => {
+    document
+      .querySelector(".btnShowCard")
+      .addEventListener("click", function() {
+        generateRandomCard();
+        generateRandomSuit();
+      });
+  };
 
-  document.querySelector(".number").innerHTML = randomNumber;
+  //function to select random number
+  let generateRandomCard = () => {
+    let cardsArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+    //Selects random card from possibleCards array and sets HTML content of the element with class ".number" to the selected card
+    let randomNumber =
+      cardsArray[Math.floor(Math.random() * cardsArray.length)];
+    document.querySelector(".number").innerHTML = randomNumber;
+  };
 
-  //select random suit for the suit on top
-  let suitsArray = ["♦", "♥", "♠", "♣"];
-  let randomSuit = suitsArray[Math.floor(Math.random() * suitsArray.length)];
+  //function to select random suit for the suit on top and  make bottom suit match the top suit
+  let generateRandomSuit = () => {
+    let suitsArray = ["♦", "♥", "♠", "♣"];
+    let randomSuit = suitsArray[Math.floor(Math.random() * suitsArray.length)];
 
-  let chosenSuit = (document.querySelector(".suit-top").innerHTML = randomSuit);
+    // Set color based on the selected suit
+    let colorOfSuit =
+      randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
 
-  //bottom suit needs to match suit on top
-  let sameSuitBottom = (document.querySelector(
-    ".suit-bottom"
-  ).innerHTML = chosenSuit);
+    document.querySelector(".suit-top").innerHTML = randomSuit;
+    document.querySelector(".suit-bottom").innerHTML = randomSuit;
+    document.querySelector(".suit-top").style.color = colorOfSuit;
+    document.querySelector(".suit-bottom").style.color = colorOfSuit;
+  };
 
-  /*let colorArray = ["#FF0000", "#000000"];
-  let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-  document.querySelector(".suit-top").innerHTML = randomColor;*/
+  showRandomCard();
 };
